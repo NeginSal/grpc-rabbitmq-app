1e4824243af06d6423d81bfac3f60036f6748451
NeginSal <neginsaljooghi@gmail.com>
2025-07-30 15:13:49 +0330

    feat:implement gRPC server

--- main.go
+++ main.go
@@ -1,7 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net"
 
-func main (){
-	fmt.Println("Hello,world")
+	"github.com/NeginSal/grpc-rabbitmq-app/grpc-rabbitmq-app/proto/orderpb"
+	"github.com/NeginSal/grpc-rabbitmq-app/order"
+	"google.golang.org/grpc"
+)
+
+func main() {
+	lis, err := net.Listen("tcp", ":50051")
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+
+	grpcServer := grpc.NewServer()
+
+	orderpb.RegisterOrderServiceServer(grpcServer, &order.Server{})
+
+	log.Println("gRPC server running on port 50051...")
+	if err := grpcServer.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
 }
